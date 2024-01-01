58de1b593d6d907659113e2cc5ce399087b93abd
Mudoker <huuquoc7603@gmail.com>
2024-01-01 22:29:16 +0700

    feat!: Message Bubble

---
+++ src/components/chat/message/bubble/msg.bubble.view.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+import { styles } from './msg.bubble.style'
+
+interface MessageBubbleProps {
+  content: string
+  sender: number
+  createdTime: Date
+}
+
+export const MessageBubble: React.FC<MessageBubbleProps> = ({ content, sender, createdTime }) => {
+  const isSender1 = sender !== 1
+
+  return (
+    <View>
+      <View style={[styles.messageContainer, isSender1 ? styles.sender1 : styles.sender2]}>
+        <Text style={isSender1 ? styles.sender1Text : styles.sender2Text}>
+          ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+        </Text>
+      </View>
+      <Text style={[styles.sentTime, isSender1 ? styles.sender1Time : styles.sender2Time]}>
+        Sent 10:21
+      </Text>
+    </View>
+  )
+}
--- src/components/chat/message/chat.msg.view.tsx
+++ src/components/chat/message/chat.msg.view.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { ScrollView, Text } from 'react-native'
-
+import { MessageBubble } from './bubble/msg.bubble.view'
 export const ChatMessageView = () => {
   return (
     <ScrollView>
-        
+<MessageBubble />
     </ScrollView>
   )
 }
