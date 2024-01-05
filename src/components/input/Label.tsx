cfefa38de43bc837a7e2a5e4cbabb25df3812df7
Nhung <nhungmaitran1412@gmail.com>
2024-01-05 21:45:18 +0700

    Fixing

--- src/components/input/Label.tsx
+++ src/components/input/Label.tsx
@@ -1,4 +1,3 @@
-import { FC } from 'react'
 import { Text, StyleSheet, View } from 'react-native'
 
 import { Constant } from '../../../components/Constant'
@@ -23,7 +22,7 @@ interface LabelProps {
   labelStyle?: any
 }
 
-export const Label: FC<LabelProps> = (props) => {
+export const Label = (props: LabelProps) => {
   return (
     <View style={LabelStyle.container}>
       <Text style={[LabelStyle.label, props.labelStyle]}>{props.label}</Text>
