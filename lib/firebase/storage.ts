e81a4a28eb13bf6240137e83b52bf4d8dc3e4bb4
hakhanhne <s3877707@rmit.edu.vn>
2024-01-08 00:51:19 +0700

    firebase storage - upload image & get download url

--- lib/firebase/storage.ts
+++ lib/firebase/storage.ts
@@ -1,6 +1,57 @@
-import { ref, getDownloadURL } from "firebase/storage";
-
 import { storage } from "@/lib/firebase/firebase";
 
-async function uploadImage(image: any) {}
-// Replace the two functions above
+import {ref, uploadBytesResumable, getDownloadURL, uploadBytes } from "firebase/storage";
+import { ToastAndroid } from "react-native/types";
+
+async function uploadImage(imageUri: any, imageRef: string) {
+  const response = await fetch(imageUri);
+  const blob = await response.blob();
+  // const fileRef = ref(storage, `images/user_avatars/${userid}.jpg`);
+  const fileRef = ref(storage, imageRef);
+  const uploadTask = uploadBytesResumable(fileRef, blob);
+
+  let imgDownloadUrl : string | undefined;
+
+  uploadTask.on('state_changed', 
+    (snapshot) => {
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      // const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      // console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case 'paused':
+          ToastAndroid.show("Upload paused", ToastAndroid.SHORT);
+          break;
+      }
+    }, 
+    (error) => {
+      switch (error.code) {
+        case 'storage/unauthorized':
+          ToastAndroid.show("Upload failed, unthorized", ToastAndroid.SHORT);
+          // User doesn't have permission to access the object
+          break;
+        case 'storage/canceled':
+          // User canceled the upload
+          ToastAndroid.show("Upload canceled", ToastAndroid.SHORT);
+          break;
+        case 'storage/unknown':
+          // Unknown error occurred, inspect error.serverResponse
+          ToastAndroid.show("Upload failed, unknow error", ToastAndroid.SHORT);
+          break;
+      }
+      
+    }, 
+    () => {
+      // Handle successful uploads on complete
+      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+      getDownloadURL(uploadTask.snapshot.ref)
+      .then((downloadUrl) => {
+        imgDownloadUrl = downloadUrl;
+      })
+      .catch((e) => {
+        ToastAndroid.show("Cannot get download url. Please try again", ToastAndroid.SHORT);
+      })
+    }
+  );
+  return imgDownloadUrl;
+}
