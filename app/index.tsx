71b84396daba4fa9c932ae3faa547b1efbc83375
Mudoker <huuquoc7603@gmail.com>
2024-01-12 21:52:09 +0700

    refactor!: Clean unused imports + codes

--- app/index.tsx
+++ app/index.tsx
@@ -1,32 +1,18 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { useRouter } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
-import { StatusBar } from 'expo-status-bar'
 import { useEffect, useState } from 'react'
-import { StyleSheet, Text, ToastAndroid, View } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
 import { Provider as PageProvider } from 'react-native-paper'
 import { Provider as ReduxProvider } from 'react-redux'
 
-import { Colors } from '@/components/Colors'
-import AppNavigator from '@/src/AppNavigator'
-import { getScreenSize } from '@/src/common/helpers/default-device-value.helper'
-import FullScreenCard from '@/src/components/cards/FullScreenCard'
-import ServiceCardLarge from '@/src/components/cards/ServiceCardLarge'
-import ServiceCardTextInside from '@/src/components/cards/ServiceCardTextInside'
-import { UserProfileScreen } from '@/src/screens/ProfileScreen'
 import { store } from '@/store'
 
 import { CallControllerScreen } from '../src/screens/CallControllerScreen'
 
 // Get the full width and height of the screen
-const { width: screenWidth } = getScreenSize()
 
 SplashScreen.preventAutoHideAsync()
 
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false)
-  const router = useRouter()
 
   useEffect(() => {
     const prepare = async () => {
@@ -73,13 +59,3 @@ export default function App() {
     </ReduxProvider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: screenWidth,
-    backgroundColor: Colors.cream,
-    alignItems: 'center',
-    overflow: 'scroll'
-  }
-})
