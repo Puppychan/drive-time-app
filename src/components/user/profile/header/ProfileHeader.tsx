7bffa46a993489c88e386c62d30305710f4062e7
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-01-22 16:52:11 +0700

    fix: update broken ProfileHeader structure

--- src/components/user/profile/header/ProfileHeader.tsx
+++ src/components/user/profile/header/ProfileHeader.tsx
@@ -7,29 +7,30 @@ import { TouchableOpacity } from 'react-native-gesture-handler'
 import { AntDesign } from '@expo/vector-icons';
 import { auth } from '@/lib/firebase/firebase'
 
-export const ProfileHeader = ({authUser, avatarOnPress}) => {
-  const [image, setImage] = useState<string |null>(null);
+export const ProfileHeader = ({ avatarOnPress }: {
+  avatarOnPress: () => void
+}) => {
+  const [image, setImage] = useState<string | null>(null);
 
   useEffect(() => {
     console.log(auth.currentUser?.photoURL)
     setImage(auth.currentUser?.photoURL ?? null)
   }, [image])
 
-export const ProfileHeader = ({authUser} : any) => {
   return (
     <View style={styles.headerContainer}>
       <View>
-        <Text style={styles.userNameTxt}>{ "Johnny Sin"}</Text>
+        <Text style={styles.userNameTxt}>{"Johnny Sin"}</Text>
 
         <View style={styles.userRatingContainer}>
           <Image style={styles.ratingImg} source={require('../../../../../assets/ic_star.png')} />
           <Text style={styles.userRatingTxt}> 5.0 </Text>
         </View>
       </View>
-      <View style={{alignItems: 'flex-end'}}>
-        <Image 
+      <View style={{ alignItems: 'flex-end' }}>
+        <Image
           style={styles.userProfileImage}
-          source={image ? { uri: image } : require('@/assets/user_profile.jpg') } 
+          source={image ? { uri: image } : require('@/assets/user_profile.jpg')}
         />
         <TouchableOpacity
           onPress={avatarOnPress}
